36c8375ef2db22b37f2bf5210b3d74e62e5f2faf
HugoVinicius <hugo.desenvolvimento@outlook.com>
2019-11-24 22:18:07 -0300

    Melhorando desempenho js

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,9 @@ var urlsToCache_ = [
     'login.html',
     '/stylesheets/font-awesome.min.css',
     '/stylesheets/style.css',
-    '/javascripts/jquery-3.3.1.slim.min.js'
+    '/javascripts/jquery-3.3.1.slim.min.js',
+    'javascripts/popper.min.js',
+    'javascripts/bootstrap.min.js'
 ];
 
 var version = 'v15';
